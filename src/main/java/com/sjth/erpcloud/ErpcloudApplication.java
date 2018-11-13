bd5f8377502f2e7602f5a7015465d8d266d64a88
Godpoison <1062265875@qq.com>
2018-11-13 13:58:38 +0800

    web框架

---
+++ src/main/java/com/sjth/erpcloud/ErpcloudApplication.java
@@ -0,0 +1,12 @@
+package com.sjth.erpcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpcloudApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sjth/erpcloud/ErpcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sjth.erpcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErpcloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
